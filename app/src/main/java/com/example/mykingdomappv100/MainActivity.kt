0b3a174db0314e5e7ef6c1489dfc986c69d9f914
Emmanuel Perez Hernandez <epere328@fiu.edu>
2020-05-23 21:54:11 -0400

    Fixed button to go to new activity correctly

--- app/src/main/java/com/example/mykingdomappv100/MainActivity.kt
+++ app/src/main/java/com/example/mykingdomappv100/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.example.mykingdomappv100
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,21 +22,6 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_favorites, R.id.navigation_settings))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-        /*Adding video button and its function when in home Tab xxx
-        try {
-            val vidButton: Button = findViewById<Button>(R.id.videoButton)
-            vidButton.setOnClickListener(View.OnClickListener {
-                @Override
-                fun onClick(view: View) {
-                    val intent = Intent(getApplicationContext(), VideoActivity::class.java)
-                    startActivity(intent)
-                }
-            })
-        }
-        catch (e:Exception) {
-            println("NOOOOOPPPPEEE")
-        }*/
 
     }
 }
--- app/src/main/java/com/example/mykingdomappv100/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mykingdomappv100/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -13,7 +14,7 @@ import com.example.mykingdomappv100.R
 import com.example.mykingdomappv100.VideoActivity
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -30,14 +31,13 @@ class HomeFragment : Fragment() {
             textView.text = it
         })
         //Button in Home
-        root.videoButton.setOnClickListener(View.OnClickListener {
-            @Override
-            fun onClick(view: View) {
-                println("YEEESSSSS")
-                val intent = Intent(context, VideoActivity::class.java)
-                startActivity(intent)
-            }
-        })
+        val vidButton: Button = root.findViewById<Button>(R.id.videoButton)
+        vidButton.setOnClickListener(this)
         return root
     }
+    override fun onClick(view: View) {
+        val intent = Intent(context, VideoActivity::class.java)
+        startActivity(intent)
+    }
+
 }

44060381eb421a2ca6c4905c88a290ca6c59a255
Emmanuel Perez Hernandez <epere328@fiu.edu>
2020-05-28 16:27:33 -0400

    Converted Videoview to Exoplayer, using HlsMediaSource to play m3u8 videos.

--- app/src/main/java/com/example/mykingdomappv100/VideoActivity.kt
+++ app/src/main/java/com/example/mykingdomappv100/VideoActivity.kt
@@ -5,14 +5,21 @@ import android.graphics.PixelFormat
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
-import android.widget.MediaController
-import android.widget.VideoView
-import kotlinx.android.synthetic.main.live_video_player.*
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+
 
 class VideoActivity : Activity() {
 
-    //Initializing videoView
-    private lateinit var vidView:VideoView
+    //Initializing the view for the video
+    //private lateinit var vidView:VideoView~
+    private lateinit var playerView:PlayerView
 
     //URL of channel(s): Testing URL
     private val videoURL:String = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
@@ -25,8 +32,11 @@ class VideoActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.live_video_player)
 
-        //VideoView is VideoView
-        vidView = findViewById(R.id.videoView)
+        //VideoView is VideoView~
+        //vidView = findViewById(R.id.videoView)~
+
+        //Player view
+        playerView = findViewById(R.id.playerView)
 
         //Video Play
         playVideo()
@@ -37,23 +47,42 @@ class VideoActivity : Activity() {
             getWindow().setFormat(PixelFormat.TRANSLUCENT)
 
             //Set Media Controller
-            val mediaController = MediaController(this)
-            mediaController.setAnchorView(videoView)
+            //val mediaController = MediaController(this)~
+            //mediaController.setAnchorView(videoView)~
+            val player = SimpleExoPlayer.Builder(this).build()
+
             //Url to Uri
             val videoUri:Uri = Uri.parse(videoURL)
             //Set Media Controllerto videoview
-            vidView.setMediaController(mediaController)
+            //vidView.setMediaController(mediaController)~
+            playerView.setPlayer(player)
             //Set video uri
-            vidView.setVideoURI(videoUri)
+            //vidView.setVideoURI(videoUri)~
 
-            vidView.requestFocus()
-            vidView.setOnPreparedListener(MediaPlayer.OnPreparedListener {
+            // Produces DataSource instances through which media data is loaded.
+            val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
+                this,
+                Util.getUserAgent(this, "MyKindomApp")
+            )
+            // This is the MediaSource representing the media to be played.
+            val videoSource: MediaSource = HlsMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(videoUri)
+            // Prepare the player with the source.
+            player.prepare(videoSource)
+            //Play video
+            //player.setPlayWhenReady(true)
+
+            //When donde: !!!!!IMPORTANT!!!!!
+            //player.release()
+
+            //vidView.requestFocus()~
+            /*vidView.setOnPreparedListener(MediaPlayer.OnPreparedListener {~
                 @Override
                 fun onPrepared(mp:MediaPlayer) {
                     //Start to play video
                     mp.start()
                 }
-            })
+            })~*/
         }
         catch (e:Exception) {
             println("Error: "+e)

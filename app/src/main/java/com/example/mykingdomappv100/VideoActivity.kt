dc9b89cb42f3c08ec92df9eddce85b59243a047e
Emmanuel Perez Hernandez <epere328@fiu.edu>
2020-05-29 00:14:00 -0400

    Made video background black as well as hide status bar and navigation bar when watching the video

--- app/src/main/java/com/example/mykingdomappv100/VideoActivity.kt
+++ app/src/main/java/com/example/mykingdomappv100/VideoActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.graphics.PixelFormat
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
@@ -69,4 +70,19 @@ class VideoActivity : Activity() {
         player.release()
     }
 
+    //Flags to hide navigation and status bar
+    private fun hideSystemBars():Int {
+        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
+
+    //When resuming
+    override fun onResume() {
+        super.onResume()
+        getWindow().getDecorView().setSystemUiVisibility(hideSystemBars())
+    }
 }

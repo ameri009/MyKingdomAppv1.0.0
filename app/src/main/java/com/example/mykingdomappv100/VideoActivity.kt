eacd2be9de288c76567a4154e1cae958f4804706
ameri009 <andre.merizalde05@gmail.com>
2020-05-28 20:23:20 -0500

    NOT FOR USAGE // Files have been deprecated

--- app/src/main/java/com/example/mykingdomappv100/VideoActivity.kt
+++ app/src/main/java/com/example/mykingdomappv100/VideoActivity.kt
@@ -10,20 +10,108 @@ import android.widget.MediaController
 import android.widget.Toast
 import androidx.annotation.Nullable
 import android.widget.VideoView
-import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.DefaultRenderersFactory
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.dash.DashMediaSource
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 import kotlinx.android.synthetic.main.live_video_player.*
 import java.lang.Exception
 
-class VideoActivity : Activity() {
+class VideoActivity : AppCompatActivity(), Player.EventListener {
+    override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters?) {
+    }
 
-    //Initializing videoView
-    private lateinit var vidView:VideoView
+    override fun onTracksChanged(trackGroups: TrackGroupArray?, trackSelections: TrackSelectionArray?) {
+    }
 
-    //URL of channel(s):
-    private val videoURL:String = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
+    override fun onPlayerError(error: ExoPlaybackException?) {
+    }
 
-    //Add Progressbar!
+    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+        if (playbackState == Player.STATE_BUFFERING)
+            progressBar.visibility = View.VISIBLE
+        else if (playbackState == Player.STATE_READY)
+            progressBar.visibility = View.INVISIBLE
+    }
 
+    override fun onLoadingChanged(isLoading: Boolean) {
+    }
+
+    override fun onPositionDiscontinuity() {
+    }
+
+    override fun onRepeatModeChanged(repeatMode: Int) {
+    }
+
+    override fun onTimelineChanged(timeline: Timeline?, manifest: Any?) {
+    }
+
+    private lateinit var simpleExoplayer: SimpleExoPlayer
+    private var playbackPosition = 0L
+    private val dashUrl = "http://rdmedia.bbc.co.uk/dash/ondemand/bbb/2/client_manifest-separate_init.mpd"
+    private val bandwidthMeter by lazy {
+        DefaultBandwidthMeter()
+    }
+    private val adaptiveTrackSelectionFactory by lazy {
+        AdaptiveTrackSelection.Factory(bandwidthMeter)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.live_video_player)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initializeExoplayer()
+    }
+
+    override fun onStop() {
+        releaseExoplayer()
+        super.onStop()
+    }
+
+    private fun initializeExoplayer() {
+        simpleExoplayer = ExoPlayerFactory.newSimpleInstance(
+            DefaultRenderersFactory(this),
+            DefaultTrackSelector(adaptiveTrackSelectionFactory),
+            DefaultLoadControl()
+        )
+
+        prepareExoplayer()
+        simpleExoPlayerView.player = simpleExoplayer
+        simpleExoplayer.seekTo(playbackPosition)
+        simpleExoplayer.playWhenReady = true
+        simpleExoplayer.addListener(this)
+    }
+
+    private fun releaseExoplayer() {
+        playbackPosition = simpleExoplayer.currentPosition
+        simpleExoplayer.release()
+    }
+
+    private fun buildMediaSource(uri: Uri): MediaSource {
+        val dataSourceFactory = DefaultHttpDataSourceFactory("ua", bandwidthMeter)
+        val dashChunkSourceFactory = DefaultDashChunkSource.Factory(dataSourceFactory)
+        return DashMediaSource(uri, dataSourceFactory, dashChunkSourceFactory, null, null)
+    }
+
+    private fun prepareExoplayer() {
+        val uri = Uri.parse(dashUrl)
+        val mediaSource = buildMediaSource(uri)
+        simpleExoplayer.prepare(mediaSource)
+    }
+}
+
+    /*
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.live_video_player)
@@ -52,7 +140,7 @@ class VideoActivity : Activity() {
                 @Override
                 fun onPrepared(mp:MediaPlayer) {
                     //Start to play video
-                    vidView.start()
+                    mp.start()
                 }
             })
         }
@@ -62,3 +150,7 @@ class VideoActivity : Activity() {
     }
 
 }
+
+
+
+     */

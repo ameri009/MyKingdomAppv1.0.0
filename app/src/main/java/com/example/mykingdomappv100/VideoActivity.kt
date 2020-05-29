b9bef1880cda85319e396a6cf422a7ecee703f18
Emmanuel Perez Hernandez <epere328@fiu.edu>
2020-05-28 21:37:22 -0400

    Clean up comments and un-used lines

--- app/src/main/java/com/example/mykingdomappv100/VideoActivity.kt
+++ app/src/main/java/com/example/mykingdomappv100/VideoActivity.kt
@@ -2,12 +2,10 @@ package com.example.mykingdomappv100
 
 import android.app.Activity
 import android.graphics.PixelFormat
-import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
-import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DataSource
@@ -18,23 +16,17 @@ import com.google.android.exoplayer2.util.Util
 class VideoActivity : Activity() {
 
     //Initializing the view for the video and video
-    //private lateinit var vidView:VideoView~
     private lateinit var playerView:PlayerView
     private lateinit var player:SimpleExoPlayer
 
     //URL of channel(s): Testing URL
     private val videoURL:String = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
-    //Testing for
-    //private val videoURL:String = "https://law.duke.edu/cspd/contest/videos/Framed-Contest_Documentaries-and-You.mp4"
 
     //Add Progressbar!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.live_video_player)
-
-        //VideoView is VideoView~
-        //vidView = findViewById(R.id.videoView)~
 
         //Player view
         playerView = findViewById(R.id.playerView)
@@ -48,17 +40,12 @@ class VideoActivity : Activity() {
             getWindow().setFormat(PixelFormat.TRANSLUCENT)
 
             //Set Media Controller
-            //val mediaController = MediaController(this)~
-            //mediaController.setAnchorView(videoView)~
             player = SimpleExoPlayer.Builder(this).build()
 
             //Url to Uri
             val videoUri:Uri = Uri.parse(videoURL)
             //Set Media Controllerto videoview
-            //vidView.setMediaController(mediaController)~
             playerView.setPlayer(player)
-            //Set video uri
-            //vidView.setVideoURI(videoUri)~
 
             // Produces DataSource instances through which media data is loaded.
             val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
@@ -70,20 +57,6 @@ class VideoActivity : Activity() {
                 .createMediaSource(videoUri)
             // Prepare the player with the source.
             player.prepare(videoSource)
-            //Play video
-            //player.setPlayWhenReady(true)
-
-            //When donde: !!!!!IMPORTANT!!!!!
-            //player.release()
-
-            //vidView.requestFocus()~
-            /*vidView.setOnPreparedListener(MediaPlayer.OnPreparedListener {~
-                @Override
-                fun onPrepared(mp:MediaPlayer) {
-                    //Start to play video
-                    mp.start()
-                }
-            })~*/
         }
         catch (e:Exception) {
             println("Error: "+e)
